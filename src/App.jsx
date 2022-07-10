c6d956588909d36a6501302eb2c7dcdc5c9f28a9
SREEHARI P V <sreehariv07@gmail.com>
2022-07-10 12:49:16 +0530

    connected the about page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { DataProvider } from "./utils/DataContext";
+import AboutPage from "./pages/AboutPage/AboutPage";
 import "./App.css";
 import HomePage from "./pages/HomePage/HomePage";
 import Navbar from "./components/Navbar/Navbar";
@@ -14,6 +15,7 @@ function App() {
           <div className="main-container">
             <Routes>
               <Route element={<HomePage />} path="/" exact />
+              <Route element={<AboutPage />} path="/about" />
             </Routes>
           </div>
         </Router>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,14 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import HamburgerButton from "../HamburgerButton/HamburgerButton";
+// import HamburgerButton from "../HamburgerButton/HamburgerButton";
 import "./Navbar.css";
 
 const Navbar = () => {
   const [sidebar, setSidebar] = useState(false);
   console.log(sidebar);
+  const toggleSideNavbar = () => {
+    setSidebar((prevState) => !prevState);
+  };
   const sidenavState = () => {
     return sidebar ? "nav-links active" : "nav-links";
   };
@@ -14,20 +17,25 @@ const Navbar = () => {
       <div className="brand-name">
         <h1>DigiLib</h1>
       </div>
-      <div
-        className="hamburger-button"
-        onClick={() => setSidebar((prevState) => !prevState)}
-      >
+      <div className="hamburger-button" onClick={toggleSideNavbar}>
         <span></span>
         <span></span>
         <span></span>
       </div>
       {/*<HamburgerButton sidebar={sidebar} setSidebar={setSidebar} />*/}
-      <div className={sidebar ? "nav-links active" : "nav-links"}>
-        <Link to="/">Home</Link>
-        <Link to="/">Home</Link>
-        <Link to="/">Home</Link>
-        <Link to="/">Home</Link>
+      <div className={sidenavState()}>
+        <Link onClick={toggleSideNavbar} to="/">
+          Home
+        </Link>
+        <Link onClick={toggleSideNavbar} to="/about">
+          About
+        </Link>
+        <Link onClick={toggleSideNavbar} to="/">
+          Home
+        </Link>
+        <Link onClick={toggleSideNavbar} to="/">
+          Home
+        </Link>
       </div>
     </nav>
   );
