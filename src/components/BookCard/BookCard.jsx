c4df38b05386adc5b01f8446a06698d7e713f4d8
SREEHARI P V <sreehariv07@gmail.com>
2022-07-24 20:31:55 +0530

    navigation in BookCard.js

--- src/components/BookCard/BookCard.jsx
+++ src/components/BookCard/BookCard.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import "./BookCard.css";
 const BookCard = (props) => {
+  let navigate = useNavigate();
   return (
-    <div className="book-card">
+    <div className="book-card" onClick>
       <div className="image-wrapper">
         <img className="book-image" src={props.imgurl} alt="book-image" />
       </div>
---
+++ src/components/SinglePageBook/SinglePageBook.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./SinglePageBook.css";
+
+const SinglePageBook = () => {
+  return <div>SinglePageBook</div>;
+};
+
+export default SinglePageBook;
